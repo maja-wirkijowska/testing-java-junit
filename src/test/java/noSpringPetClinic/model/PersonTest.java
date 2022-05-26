d7a0a643d354eae026c6a69f3162b80cf30dd1db
Maja Wirkijowska <maja.wirkijowska@gmail.com>
2022-05-26 15:20:03 -0700

    repeated test with dependecy injection, very limited

--- src/test/java/noSpringPetClinic/model/PersonTest.java
+++ src/test/java/noSpringPetClinic/model/PersonTest.java
@@ -1,10 +1,7 @@
 package noSpringPetClinic.model;
 
 import noSpringPetClinic.ModelTest;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,5 +33,10 @@ class PersonTest implements ModelTest {
     @DisplayName("MyRepeatedTest")
     void myRepeatedTest() {
         // todo - impl
+    }
+
+    @RepeatedTest(5)
+    void myRepeatedTestWithDI(TestInfo testInfo, RepetitionInfo repetitionInfo) {
+        System.out.println(testInfo.getDisplayName() + " : " + repetitionInfo.getCurrentRepetition());
     }
 }
