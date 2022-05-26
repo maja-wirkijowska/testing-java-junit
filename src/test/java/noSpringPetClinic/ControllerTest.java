2489bb20d5361831223777f2ed2a86278d44d4c5
Maja Wirkijowska <maja.wirkijowska@gmail.com>
2022-05-26 15:11:05 -0700

    default controller and model tests

--- src/test/java/noSpringPetClinic/ControllerTest.java
+++ src/test/java/noSpringPetClinic/ControllerTest.java
@@ -1,7 +1,16 @@
 package noSpringPetClinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("controller")
 public interface ControllerTest {
+
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("default controller beforeAll()");
+    }
+
 }
--- src/test/java/noSpringPetClinic/ModelTest.java
+++ src/test/java/noSpringPetClinic/ModelTest.java
@@ -1,7 +1,16 @@
 package noSpringPetClinic;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @Tag("model")
 public interface ModelTest {
+
+    @BeforeAll
+    default void beforeAll() {
+        System.out.println("default model beforeAll()");
+    }
+
 }
