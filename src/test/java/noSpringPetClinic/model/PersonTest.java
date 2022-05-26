e8931ed074989174c0e0a569ef84b74438dfe2df
Maja Wirkijowska <maja.wirkijowska@gmail.com>
2022-05-26 09:35:21 -0700

    PersonTest, groupAssertions with lambda expressions

---
+++ src/test/java/noSpringPetClinic/model/PersonTest.java
@@ -0,0 +1,31 @@
+package noSpringPetClinic.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTest {
+
+    @Test
+    void groupAssertions() {
+        //given
+        Person person = new Person(1L, "Joe", "Buck");
+
+        //then
+        assertAll("Test Props Set",
+                () -> assertEquals(person.getFirstName(), "Joe"),
+                () -> assertEquals(person.getLastName(), "Buck"));
+    }
+
+    @Test
+    void groupAssertionsMsg() {
+        //given
+        Person person = new Person(1L, "Joe", "Buck");
+
+        //then
+        assertAll("Test Props Set",
+                () -> assertEquals(person.getFirstName(), "Joe", "First Name Failed"),
+                () -> assertEquals(person.getLastName(), "Buck", "Last Name Failed"));
+    }
+
+}
