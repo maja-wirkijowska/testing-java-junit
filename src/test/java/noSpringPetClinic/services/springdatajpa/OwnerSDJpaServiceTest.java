589fc4dff0c23acfbbf950910e675fa7d3f5d226
Maja Wirkijowska <maja.wirkijowska@gmail.com>
2022-05-26 09:57:32 -0700

    @Disabled OwnerSDJpaServiceTest

---
+++ src/test/java/noSpringPetClinic/services/springdatajpa/OwnerSDJpaServiceTest.java
@@ -0,0 +1,48 @@
+package noSpringPetClinic.services.springdatajpa;
+
+import noSpringPetClinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@Disabled(value = "Disabled until we learn Mocking") // annotation can also be used on individual methods
+class OwnerSDJpaServiceTest {
+
+    OwnerSDJpaService service;
+
+    @BeforeEach
+    void setUp() {
+        service = new OwnerSDJpaService(null, null, null);
+    }
+
+    @Test
+    void findByLastName() {
+        Owner owner = service.findByLastName("Buck");
+    }
+
+    @Test
+    void findAllByLastNameLike() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+}
