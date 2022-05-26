2315f4aa8215af03b8bc37eb9a09fc411ace053d
Maja Wirkijowska <maja.wirkijowska@gmail.com>
2022-05-26 15:15:13 -0700

    repeated tests

--- src/test/java/noSpringPetClinic/model/PersonTest.java
+++ src/test/java/noSpringPetClinic/model/PersonTest.java
@@ -1,6 +1,8 @@
 package noSpringPetClinic.model;
 
 import noSpringPetClinic.ModelTest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -30,4 +32,10 @@ class PersonTest implements ModelTest {
                 () -> assertEquals("Buck", person.getLastName(), "Last Name Failed"));
     }
 
+    @RepeatedTest(value = 10, name = "{displayName} : {currentRepetition} / {totalRepetitions}")
+    @DisplayName("MyRepeatedTest")
+    @Test
+    void myRepeatedTest() {
+        // todo - impl
+    }
 }
