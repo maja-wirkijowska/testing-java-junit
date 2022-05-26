47e3899858ab9ee5eea1ed0953042e5de2060594
Maja Wirkijowska <maja.wirkijowska@gmail.com>
2022-05-26 10:07:09 -0700

    IndexController, @DisplayName annotation

--- src/test/java/noSpringPetClinic/controllers/IndexControllerTest.java
+++ src/test/java/noSpringPetClinic/controllers/IndexControllerTest.java
@@ -1,6 +1,8 @@
 package noSpringPetClinic.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,6 +16,7 @@ class IndexControllerTest {
         controller = new IndexController();
     }
 
+    @DisplayName("Test Proper View Name is returned for Index page")
     @Test
     void index() {
         assertEquals("index", controller.index());
@@ -25,6 +28,7 @@ class IndexControllerTest {
     }
 
     @Test
+    @DisplayName("Test exception")
     void oupsHandler() {
         assertTrue("notimplemented".equals(controller.oupsHandler()), () -> "This is some expensive " +
                 "message to build " + "for my test");
