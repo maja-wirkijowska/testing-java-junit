3353d1dbe675b1e40c1584aed88d8ee383960038
Maja Wirkijowska <maja.wirkijowska@gmail.com>
2022-05-27 10:22:06 -0700

    deleteById() for VetSDJpaService

---
+++ src/test/java/noSpringPetClinic/services/springdatajpa/VetSDJpaServiceTest.java
@@ -0,0 +1,25 @@
+package noSpringPetClinic.services.springdatajpa;
+
+import noSpringPetClinic.repositories.VetRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class VetSDJpaServiceTest {
+
+    @Mock
+    VetRepository vetRepository;
+
+    @InjectMocks
+    VetSDJpaService service;
+
+    @Test
+    void deleteById() {
+        service.deleteById(1L);
+        verify(vetRepository, times(1)).deleteById(1L);
+    }
+}
