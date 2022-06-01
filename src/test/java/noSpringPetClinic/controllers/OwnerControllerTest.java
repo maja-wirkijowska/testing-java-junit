d187ddeef38c17c852ba1d40bfd8d9819852562a
Maja Wirkijowska <maja.wirkijowska@gmail.com>
2022-06-01 09:51:40 -0700

    mockito argument capture

--- src/test/java/noSpringPetClinic/controllers/OwnerControllerTest.java
+++ src/test/java/noSpringPetClinic/controllers/OwnerControllerTest.java
@@ -5,9 +5,15 @@ import noSpringPetClinic.model.Owner;
 import noSpringPetClinic.services.OwnerService;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.BDDMockito.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -25,6 +31,34 @@ class OwnerControllerTest {
 
     @Mock
     BindingResult bindingResult;
+
+    @Captor
+    ArgumentCaptor<String> stringArgumentCaptor;
+
+    @Test
+    void processFindFormWildCardStringAnnotation() {
+        //given
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        List<Owner> ownerList = new ArrayList<>();
+        given(ownerService.findAllByLastNameLike(stringArgumentCaptor.capture())).willReturn(ownerList);
+        //when
+        String viewName = controller.processFindForm(owner, bindingResult, null);
+        //then
+        assertThat("%Buck%").isEqualToIgnoringCase(stringArgumentCaptor.getValue());
+    }
+
+    @Test
+    void processFindFormWildCardString() {
+        //given
+        Owner owner = new Owner(1L, "Joe", "Buck");
+        List<Owner> ownerList = new ArrayList<>();
+        final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        given(ownerService.findAllByLastNameLike(captor.capture())).willReturn(ownerList);
+        //when
+        String viewName = controller.processFindForm(owner, bindingResult, null);
+        //then
+        assertThat("%Buck%").isEqualToIgnoringCase(captor.getValue());
+    }
 
     @Test
     void testProcessCreationFormHasErrors() {
